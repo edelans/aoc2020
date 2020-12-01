e368a272daa196776020180b75c4fb99b4ec5f86
edelans <edelans@gmail.com>
2020-12-01 23:29:47 +0100

    part 1 solved

--- day01.py
+++ day01.py
@@ -1,5 +1,7 @@
-# Python 3.x
+#!/usr/bin/env python3
+
 from aoc_utilities import Input
+import sys
 
 # day must be 2 digit
 DAY = '01'
@@ -10,13 +12,27 @@ PART1
 """
 
 def solve1(input):
-
+    for i in range(len(input)):
+        for j in range(i+1, len(input)):
+            a = int(input[i])
+            b = int(input[j])
+            if (a + b) == 2020:
+                return a * b
+    raise Exception("Sorry, no match found in your input")
 
 """
 PART 2
 """
-import itertools
 
 
-def solve2(input):
+if __name__ == '__main__':
+    if sys.argv[1] == '1':
+        res = solve1((Input(DAY).readlines()))
+        print(res)
+
+    if sys.argv[1] == '2':
+        res = solve2((Input(DAY).readlines()))
+        print(res)
+
+
 
