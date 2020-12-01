24eea88a990fc86df84fee0ae842010326d06b56
edelans <edelans@gmail.com>
2020-12-01 23:41:04 +0100

    part 2 solved

--- day01.py
+++ day01.py
@@ -24,6 +24,18 @@ def solve1(input):
 PART 2
 """
 
+def solve2(input):
+    for i in range(len(input)):
+        for j in range(i+1, len(input)):
+            for k in range(j+1, len(input)):
+                a = int(input[i])
+                b = int(input[j])
+                c = int(input[k])
+                if (a + b + c) == 2020:
+                    return a * b * c
+    raise Exception("Sorry, no match found in your input")
+
+
 
 if __name__ == '__main__':
     if sys.argv[1] == '1':
