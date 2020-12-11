4b7dd116e6f9380f1757e61cd7eb2614bfb06d0a
edelans <edelans@gmail.com>
2020-12-11 08:55:36 +0100

    add mathematical solution

--- day10.py
+++ day10.py
@@ -18,9 +18,9 @@ def solve1(data):
     delta1 = 0
     delta3 = 1  # accoutns for the end of the chain which is always 3 jolts diff
     for i in range(0, len(adapters) - 1):
-        if abs(adapters[i + 1] - adapters[i]) == 1:
+        if adapters[i + 1] - adapters[i] == 1:
             delta1 += 1
-        if abs(adapters[i + 1] - adapters[i]) == 3:
+        if adapters[i + 1] - adapters[i] == 3:
             delta3 += 1
     return delta1, delta3, delta1 * delta3
 
@@ -30,6 +30,19 @@ def solve2(data):
     for i in data:
         print(i)
     pass
+
+# part2 solved by hand : 
+"""
+dans l'input ya que des écarts de 1 ou de 3
+les écarts de 3 ne permettent pas d'alternative
+les écarts de 1 permettent différentes alternatives en fonction de leur longueur :
+  - pas d'alternative pour une suite de 2 élément
+  - 2 alternatives pour une suite de 3 elements
+  - 4 aternatives pour une suite de 4 éléments
+  - 7 alternatives pour une suite de 5 éléments
+  - yavais pas de suite plus grande que ça
+2^x*4^y*7^z avec x le nombre de suite 3 éléments écartés de 1, y celui de 4 et z celui de 5
+"""
 
 
 """
