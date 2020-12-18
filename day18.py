90623f7fa4c6af8727e90c431510913b0a9ca612
edelans <edelans@gmail.com>
2020-12-18 13:00:37 +0100

    part 2 ok

--- day18.py
+++ day18.py
@@ -31,9 +31,30 @@ def solve1(data):
     return sum([eval_line(line) for line in data])
 
 
+def eval_line2(line):
+    # resolve parentheses first
+    m = re.search(r"\((\d+(\s[+*]\s\d+)+)\)", line)
+    if m:
+        inner_parenthesis = eval_line2(m.group(1))
+        return eval_line2(line.replace(m.group(0), str(inner_parenthesis), 1))
+
+    # then eval all the additions starting from the left
+    m2 = re.search(r"\d+\s\+\s\d+", line)
+    if m2:
+        return eval_line2(line.replace(m2.group(0), str(eval(m2.group(0))), 1))
+
+    # then eval all the multiplications starting from the left
+    m3 = re.search(r"\d+\s\*\s\d+", line)
+    if m3:
+        return eval_line2(line.replace(m3.group(0), str(eval(m3.group(0))), 1))
+
+    # when there is no more operation or parenthesis
+    return int(line)
+
+
 def solve2(data):
     """Solves part2."""
-    pass
+    return sum([eval_line2(line) for line in data])
 
 
 """
@@ -55,5 +76,9 @@ if __name__ == '__main__':
         res = solve2((Input(DAY).readlines()))
         print(res)
     if len(sys.argv) > 1 and sys.argv[1] == '2t':
-        res = solve2((test_input(DAY).readlines()))
-        print(res)
+        print(eval_line2("1 + 2 * 3 + 4 * 5 + 6"))
+        print(eval_line2("1 + (2 * 3) + (4 * (5 + 6))"))
+        print(eval_line2("2 * 3 + (4 * 5)"))
+        print(eval_line2("5 + (8 * 3 + 9 + 3 * 4 * 3)"))
+        print(eval_line2("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"))
+        print(eval_line2("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"))
