1742f341204154a4388b1ae338a012e06f00192e
edelans <edelans@gmail.com>
2020-12-22 20:41:58 +0100

    part 2 ok

--- day22.py
+++ day22.py
@@ -9,12 +9,9 @@ from collections import deque
 # 2 digit day fetched from filename
 DAY = os.path.basename(__file__)[3:5]
 
-# to avoid messing up with indiced
+# to avoid messing up with indices
 P1 = 0
 P2 = 1
-
-# history as global var to help with recursions
-hist = set()
 
 
 def parser(data):
@@ -50,29 +47,32 @@ def solve1(data):
 
 
 def recursive_combat(deck):
-    print("\ngame with:")
-    print(f'deck1 = {deck[P1]}')
-    print(f'deck2 = {deck[P2]}')
-    if (tuple(deck[P1]), tuple(deck[P2])) in hist:
-        print("infinite game detected, player1 wins")
-        return P1
-    else:
+
+    # record history to avoid infinity combats
+    hist = set()
+
+    while deck[P1] and deck[P2]:
+
+        t = (tuple(deck[P1]), tuple(deck[P2]))
+        if t in hist:
+            return P1
         hist.add((tuple(deck[P1]), tuple(deck[P2])))
 
-    card1 = deck[P1].popleft()
-    card2 = deck[P2].popleft()
+        card1 = deck[P1].popleft()
+        card2 = deck[P2].popleft()
 
-    if len(deck[P1]) >= card1 and len(deck[P2]) >= card2:
-        # recursive combat
-        print("\n=== entering subgame ===\n")
-        rdeck1 = deque([deck[P1][i] for i in range(card1)])
-        rdeck2 = deque([deck[P2][i] for i in range(card2)])
-        winner = recursive_combat([rdeck1, rdeck2])
+        if len(deck[P1]) >= card1 and len(deck[P2]) >= card2:
+            rdeck1 = deque([deck[P1][i] for i in range(card1)])
+            rdeck2 = deque([deck[P2][i] for i in range(card2)])
+            winner = recursive_combat([rdeck1, rdeck2])
 
-    else:
-        # winner of the round is the player with the higher-value card
-        print("regular combat")
-        winner = P1 if card1 > card2 else P2
+        else:
+            winner = P1 if card1 > card2 else P2
+
+        winner_card = card1 if winner == P1 else card2
+        loser_card = card2 if winner == P1 else card1
+        deck[winner].append(winner_card)
+        deck[winner].append(loser_card)
 
     return winner
 
@@ -81,20 +81,7 @@ def solve2(data):
     """Solves part2."""
     deck = parser(data)
 
-    while len(deck[P1]) > 0 and len(deck[P2]) > 0:
-
-        winner = recursive_combat(deck)
-
-        card1 = deck[P1].popleft()
-        card2 = deck[P2].popleft()
-        winner_card = card1 if winner == P1 else card2
-        loser_card = card2 if winner == P1 else card1
-
-        deck[winner].append(winner_card)
-        deck[winner].append(loser_card)
-
-    print(deck[P1])
-    print(deck[P2])
+    recursive_combat(deck)
 
     return score(deck[P1]), score(deck[P2])
 
@@ -113,5 +100,13 @@ if __name__ == '__main__':
         res = solve2((Input(DAY).read()))
         print(res)
     if len(sys.argv) > 1 and sys.argv[1] == '2t':
-        res = solve2((test_input(DAY).read()))
+        # res = solve2((test_input(DAY).read()))
+        res = solve2("""Player 1:
+43
+19
+
+Player 2:
+2
+29
+14""")
         print(res)
